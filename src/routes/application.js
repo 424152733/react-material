a1b178cacf09f394786cc73cd22454312ac8e2ac
黄强 <424152733@qq.com>
2016-11-28 09:59:55 +0800

    commit some prop define

--- src/routes/application.js
+++ src/routes/application.js
@@ -3,6 +3,26 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Header from '../components/header'
 import Nav from '../components/nav'
 export default class AppRoute extends React.Component {
+	static propTypes = {
+
+	}
+
+	static contextTypes = {
+		
+	}
+
+	state = {
+
+	}
+
+	static childContextTypes = {
+
+	}
+
+	getChildContext() {
+
+	}
+
 	render() {
 		return (
 			<MuiThemeProvider>
